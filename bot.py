a8dc6ae0866d741ccf988a31d4c2c7bfff11840d
abhijit <amiabhijit54@gmail.com>
2023-01-11 02:05:08 +0530

    Changed the logic of bot as expense messages can work better than receipt images

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import requests
 import PIL.Image as Image
 import io
 import pytesseract as tess
+import numpy as np
+import cv2
+import re
 
 
 # If the script generates connection timed out error then uncomment the
@@ -17,6 +20,25 @@ with open('token.txt', 'r') as f:
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 bot = telebot.TeleBot(token)
+
+def increase_accuracy(img):
+    img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+
+    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    text = tess.image_to_string(img, lang="eng")
+    return text
+
+def extract_info(text):
+
 
 @bot.message_handler(commands=['run'])
 def send_welcome(message):
@@ -37,10 +59,14 @@ def expense_receipt(message):
     print('file.file_path =', file_info.file_path)
     receipt = bot.download_file(file_info.file_path)
     img = Image.open(io.BytesIO(receipt))
-    text = tess.image_to_string(img)
+    img.save(r'C:\Users\amiab\Projects\Expense Manager\media\img.jpg')
+    img = cv2.imread(r'C:\Users\amiab\Projects\Expense Manager\media\img.jpg')
+    text = increase_accuracy(img)
     # print(text)
     bot.reply_to(message, text)
-    # img.save(r'C:\Users\amiab\Projects\Expense Manager\media\img.pdf')
+
+
+    # img.save(r'C:\Users\amiab\Projects\Expense Manager\media\img3.jpg')
     #To download the receipt
     # with open("C:\\Users\\amiab\\Projects\Expense Manager\\media\\image.jpg", 'wb') as new_file:
     #     new_file.write(receipt)
