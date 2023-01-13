36f0893ab1272494aca485ca64c683f7eae27996
abhijit <amiabhijit54@gmail.com>
2023-01-14 03:01:06 +0530

    created untracked expenses and completed the functionality of bot

--- bot.py
+++ bot.py
@@ -1,26 +1,18 @@
 import telebot
 from datetime import datetime
 import requests
-import PIL.Image as Image
-import io
-import numpy as np
-import cv2
 import re
 
 
 # ----------If the script generates connection timed out error then uncomment the
 # ------------------------below line and execute the whole program
-requests.get('https://google.com', verify=False)
+# requests.get('https://google.com', verify=False)
 
 token = ''
 with open('token.txt', 'r') as f:
     token = f.read()
-    # print(token)
-
-# tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 bot = telebot.TeleBot(token)
-
 def expense_report(text):
     txn_keyword = ['SBI', 'Transaction', 'from', 'to', 'Paid', 'Paytm', 'Food Wallet', 'Rs', 'A/c',
                    'debited', 'credited', 'Ref', 'Ref No', 'transfer to', 'UPI', 'Txn ID', 'payment',
@@ -49,16 +41,16 @@ def expense_report(text):
 Amount : {}
 Transaction ID : N/A
 Type : {}
-To : N/A
-From : Wallet
+Source : Wallet
 Mode : Cash'''.format(dt_string, amount[0], type)
 
+        print(report)
         return report
 
     #----transaction type
 
     # debit = ['paid', 'debited']
-    credit = ['added', 'credited']
+    credit = ['added', 'credited', 'received']
     txn_type = ''
 
     if any(x.lower() in text.lower() for x in credit):
@@ -76,24 +68,37 @@ Mode : Cash'''.format(dt_string, amount[0], type)
     # print(txn_amt)
 
     #---- Reference number
-
+    txn_ref = ''
     res = re.findall(r'(?:Ref No\.?|Txn ID|ref no|Ref\:|transaction number)\s*(\d{11,12})', text)
     if len(res) > 0:
-        txn_ref = res[0]
+        txn_ref += res[0]
     else :
         txn_ref = 'N/A'
+
+    #---- Source // this part will fetch data from the database about the source.
+
+    txn_source = ''
+
+    if 'SBI'.lower() in text.lower():
+        txn_source += 'SBI'
+    elif 'Union Bank'.lower() in text.lower():
+        txn_source += 'Union Bank'
+    elif ('Food Wallet'.lower() in text.lower()) or ('Paytm'.lower() in text.lower()):
+        txn_source += 'Paytm Wallet'
+
+
+    #---- Mode
+    txn_mode = 'Cashless'
 
     report = '''Datetime : {}
 Amount : {}
 Transaction ID : {}
 Type : {}
-To : N/A
-From : Online
-Mode : UPI'''.format(dt_string, txn_amt, txn_ref, txn_type)
+Source : {}
+Mode : {}'''.format(dt_string, txn_amt, txn_ref, txn_type, txn_source, txn_mode)
 
+    print(report)
     return (report)
-
-
 
 
     # return "Processing report."
@@ -103,29 +108,6 @@ def expense_message(msg):
     txn_msg = msg.text
     detail_msg = expense_report(txn_msg)
     bot.reply_to(msg, detail_msg)
-
-
-# @bot.message_handler(content_types=['photo'])
-# def expense_receipt(message):
-#     # bot.reply_to(message, "This is a picture.")
-#     print('message.photo =', message.photo)
-#     fileID = message.photo[-1].file_id
-#     print('fileID =', fileID)
-#     file_info = bot.get_file(fileID)
-#     print('file.file_path =', file_info.file_path)
-#     receipt = bot.download_file(file_info.file_path)
-#     img = Image.open(io.BytesIO(receipt))
-#     img.save(r'C:\Users\amiab\Projects\Expense Manager\media\img.jpg')
-#     img = cv2.imread(r'C:\Users\amiab\Projects\Expense Manager\media\img.jpg')
-#     text = increase_accuracy(img)
-#     # print(text)
-#     bot.reply_to(message, text)
-
-
-    # img.save(r'C:\Users\amiab\Projects\Expense Manager\media\img3.jpg')
-    #To download the receipt
-    # with open("C:\\Users\\amiab\\Projects\Expense Manager\\media\\image.jpg", 'wb') as new_file:
-    #     new_file.write(receipt)
 
 
 bot.infinity_polling()
