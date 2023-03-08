6a294df9351fee7e3c0618dba4e46adbf7c24dc7
abhijit <amiabhijit54@gmail.com>
2023-03-09 01:07:58 +0530

    category and source are added to the database

--- connect_&_create.py
+++ connect_&_create.py
@@ -4,6 +4,8 @@ conn = sqlite3.connect('expense.db')
 
 print('opened DB successfully')
 
+####----untracked_transaction----
+
 # conn.execute('drop table if exists untracked_txn;')
 #
 # conn.execute('''create table if not exists untracked_txn (
@@ -16,23 +18,46 @@ print('opened DB successfully')
 # balance varchar(50)
 # );''')
 
+####----transaction----
 
-conn.execute('drop table if exists transactions;')
+# conn.execute('drop table if exists transactions;')
+#
+# conn.execute('''create table if not exists transactions (
+# expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
+# datetime date,
+# amount real,
+# txn_id varchar(100),
+# txn_desc varchar(200),
+# txn_type varchar(100),
+# balance real,
+# source varchar(100),
+# sub_source varchar(200),
+# category varchar(100),
+# sub_category varchar(200),
+# spent_desc varchar(300),
+# wasted boolean
+# );''')
 
-conn.execute('''create table if not exists transactions (
-expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
-datetime date,
-amount real,
-txn_id varchar(100),
-txn_desc varchar(200),
-txn_type varchar(100),
-balance real,
-source varchar(100),
-sub_source varchar(200),
-category varchar(100),
-sub_category varchar(200),
-spent_desc varchar(300),
-wasted boolean
+
+####----Category----
+
+# conn.execute('drop table if exists category;')
+#
+# conn.execute('''create table if not exists category (
+# category_id varchar(100),
+# category varchar(100),
+# sub_category varchar(200),
+# planned_budget real
+# );''')
+
+####----Source----
+
+conn.execute('drop table if exists source;')
+
+conn.execute('''create table if not exists source (
+source_id varchar(200),
+source varchar(200),
+sub_source varchar(200)
 );''')
 
 print("Table created successfully")
---
+++ csv_parser_loader.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import sqlite3
+from numpy import double
+
+# data = pd.read_csv(r'C:\Users\amiab\Projects\Expense Manager\csv_files\Category.csv')
+data = pd.read_csv(r'C:\Users\amiab\Projects\Expense Manager\csv_files\Source.csv')
+df = pd.DataFrame(data)
+
+conn = sqlite3.connect('expense.db')
+print('opened DB successfully')
+
+# Category
+
+# for row in range(len(df)):
+#     category_id = df.iloc[row]['category_id']
+#     category = df.iloc[row]['category']
+#     sub_category = df.iloc[row]['sub_category']
+#     planned_budget = df.iloc[row]['planned_budget']
+#
+#     params = (category_id, category, sub_category, planned_budget)
+#     insert_val = '''insert into category values(?, ?, ?, ?);'''
+#     conn.execute(insert_val, params)
+
+# Source
+
+for row in range(len(df)):
+    source_id = df.iloc[row]['source_id']
+    source = df.iloc[row]['source']
+    sub_source = df.iloc[row]['sub_source']
+
+    params = (source_id, source, sub_source)
+    insert_val = '''insert into source values(?, ?, ?);'''
+    conn.execute(insert_val, params)
+
+
+conn.commit()
+conn.close()
